f6583963c5c4678c5d14c102ac65d4ce28612957
Beepbloop <louisliu2096@gmail.com>
2019-10-27 04:52:40 -0400

    Louis's 2nd commit

---
+++ docScan.py
@@ -0,0 +1,101 @@
+import argparse
+from enum import Enum
+import io
+import os
+
+from google.cloud import vision
+from google.cloud.vision import types
+from PIL import Image, ImageDraw
+import json
+
+
+class FeatureType(Enum):
+    PAGE = 1
+    BLOCK = 2
+    PARA = 3
+    WORD = 4
+    SYMBOL = 5
+
+
+def draw_boxes(image, bounds, color):
+    """Draw a border around the image using the hints in the vector list."""
+    draw = ImageDraw.Draw(image)
+
+    for bound in bounds:
+        draw.polygon([
+            bound.vertices[0].x, bound.vertices[0].y,
+            bound.vertices[1].x, bound.vertices[1].y,
+            bound.vertices[2].x, bound.vertices[2].y,
+            bound.vertices[3].x, bound.vertices[3].y], None, color)
+    return image
+
+
+def get_document_bounds(image_file, feature):
+    # [START vision_document_text_tutorial_detect_bounds]
+    """Returns document bounds given an image."""
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.join(os.curdir, 'CalhackCV-a4f9f43dfda3.json')
+    client = vision.ImageAnnotatorClient()
+
+    bounds = []
+
+    with io.open(image_file, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    response = client.document_text_detection(image=image)
+    document = response.full_text_annotation
+
+    # print('hello world')
+    # print(document)
+    # Collect specified feature bounds by enumerating all document features
+    for page in document.pages:
+        for block in page.blocks:
+            for paragraph in block.paragraphs:
+                for word in paragraph.words:
+                    for symbol in word.symbols:
+                        if (feature == FeatureType.SYMBOL):
+                            bounds.append(symbol.bounding_box)
+
+                    if (feature == FeatureType.WORD):
+                        bounds.append(word.bounding_box)
+
+                if (feature == FeatureType.PARA):
+                    bounds.append(paragraph.bounding_box)
+
+            if (feature == FeatureType.BLOCK):
+                bounds.append(block.bounding_box)
+
+        if (feature == FeatureType.PAGE):
+            bounds.append(block.bounding_box)
+
+    # The list `bounds` contains the coordinates of the bounding boxes.
+    # [END vision_document_text_tutorial_detect_bounds]
+    return bounds
+
+
+def render_doc_text(filein, fileout):
+    image = Image.open(filein)
+    bounds = get_document_bounds(filein, FeatureType.PAGE)
+    draw_boxes(image, bounds, 'blue')
+    bounds = get_document_bounds(filein, FeatureType.PARA)
+    draw_boxes(image, bounds, 'red')
+    bounds = get_document_bounds(filein, FeatureType.WORD)
+    draw_boxes(image, bounds, 'yellow')
+
+    if fileout != 0:
+        image.save(fileout)
+    else:
+        image.show()
+
+
+if __name__ == '__main__':
+    # [START vision_document_text_tutorial_run_application]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('detect_file', help='The image for text detection.')
+    parser.add_argument('-out_file', help='Optional output file', default=0)
+    args = parser.parse_args()
+
+    render_doc_text(args.detect_file, args.out_file)
+    # [END vision_document_text_tutorial_run_application]
+# [END vision_document_text_tutorial]
